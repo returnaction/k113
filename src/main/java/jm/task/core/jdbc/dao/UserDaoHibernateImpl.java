1d8f626458c4487142fe1bfc8cdd91ee093380b0
returnaction <obergannv@gmail.com>
2025-02-01 05:08:42 +0300

    Ну вроде бы сделал

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,6 +1,9 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -9,34 +12,70 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
+        String sqlCreateTableQuery = "CREATE TABLE IF NOT EXISTS `katabd`.`users` (\n" +
+                "  `id` BIGINT NOT NULL AUTO_INCREMENT,\n" +
+                "  `name` VARCHAR(100) NOT NULL,\n" +
+                "  `lastName` VARCHAR(100) NOT NULL,\n" +
+                "  `age` TINYINT NOT NULL,\n" +
+                "  PRIMARY KEY (`id`));\n";
 
+        try(Session session = Util.getSessionFactory().openSession()) {
+            session.beginTransaction();
+            session.createNativeQuery(sqlCreateTableQuery).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Users table is created");
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        String sqlDropTableQeury = "DROP TABLE IF EXISTS `katabd`.`users`";
+        try(Session session = Util.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.createNativeQuery(sqlDropTableQeury).executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Users table is deleted.");
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try(Session session = Util.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            transaction.commit();
+            System.out.println("New User " + name + " " + lastName + " " + age + " saved");
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
+        try(Session session = Util.getSessionFactory().openSession()){
+            Transaction transaction = session.beginTransaction();
+            User user = session.get(User.class, id);
+            if(user != null){
+                session.delete(user);
+                System.out.println("User " + user.getId() + " " + user.getLastName() + " " + user.getAge() + " removed");
+            }
+            transaction.commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try(Session session = Util.getSessionFactory().openSession()){
+            return session.createQuery("FROM User", User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try(Session session  = Util.getSessionFactory().openSession()){
+            session.beginTransaction();
+            session.createQuery("DELETE FROM User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,15 +1,22 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-    private final String HOST = "jdbc:mysql://localhost:3306/katabd";
-    private final String USER = "root";
-    private final String PASSWORD = "root";
+    private static final String HOST = "jdbc:mysql://localhost:3306/katabd";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root";
     private Connection connection;
+
+    // имплементация с Hibernate
+    private static SessionFactory sessionFactory;
     public Util() {
         try{
             connection = DriverManager.getConnection(HOST, USER, PASSWORD);
@@ -21,5 +28,22 @@ public class Util {
 
     public Connection getConnection() {
         return connection;
+    }
+
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory == null){
+            sessionFactory = new Configuration()
+                    .setProperty("hibernate.connection.url", HOST)
+                    .setProperty("hibernate.connection.username", USER)
+                    .setProperty("hibernate.connection.password", PASSWORD)
+                    .setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+                    .setProperty("hibernate.hbm2ddl.auto", "update")
+                    .setProperty("hibernate.show_sql", "true")
+                    .setProperty("hibernate.current_session_context_class", "thread")
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        }
+
+        return sessionFactory;
     }
 }
